b8ca738f3f8eb1d9de3ccd5d6fd40bd5297386de
Esther-T <tan.esther8@gmail.com>
2021-01-04 13:39:49 -0800

    worked with jsx react js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+const name = "Esther";
+
 ReactDom.render(
   <div>
-    <h1>My favorite classic songs</h1>
+    <h1>Hello {name} :)</h1>
+    <h2>My favorite classic songs</h2>
     <ul>
       <li>Don't Stop Me Now</li>
       <li>My Life</li>
