a3990f2e177a1d7be4dd6b5dc8e9b7b920fe8d42
Esther-T <tan.esther8@gmail.com>
2021-01-04 13:29:47 -0800

    Worked with basic jsx react.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-var React = require("react");
-var ReactDom = require("react-dom");
-
-ReactDom.render(<h1>Hello World</h1>, document.getElementById("root"));
-/*
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
+ReactDom.render(
   <div>
-    <h1>Hello World!</h1>
-    <p>This is a paragraph.</p>
+    <h1>Hello World</h1>
+    <p>Here's some message</p>
   </div>,
   document.getElementById("root")
 );
-*/
