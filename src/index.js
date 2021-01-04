f1fc1f9ec9a4ae2b5aa30a4cb395cd6cf125badf
Esther-T <tan.esther8@gmail.com>
2021-01-04 15:26:26 -0800

    worked on javascript expressions in react js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,7 @@ import ReactDom from "react-dom";
 
 const fName = "Esther";
 const lName = "T";
-const currYear = "2021";
-
-//Create a react app from scratch.
-//It should display 2 paragraph HTML elements.
-//The paragraphs should say:
-//Created by YOURNAME.
-//Copyright CURRENTYEAR.
-//E.g.
-//Created by Angela Yu.
-//Copyright 2019.
+const currYear = new Date().getFullYear();
 
 ReactDom.render(
   <div>
