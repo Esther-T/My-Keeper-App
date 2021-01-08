39762f6c66d9ddee1dc9aa8518e4059954518593
Esther-T <tan.esther8@gmail.com>
2021-01-07 21:28:56 -0800

    Worked on mapping react js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Heading from "./Heading.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
+import notes from "../notes.js";
 
+function createNote(note) {
+  return <Note key={note.id} title={note.title} content={note.content} />;
+}
 function App() {
   return (
     <div>
       <Heading />
-      <Note title="This is the title" content="This is the content" />
+      {notes.map(createNote)};
       <Footer />
     </div>
   );
